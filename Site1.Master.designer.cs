56d6d269f6a450dec92a5d51e2222652fcbe3365
Scott Henkel <58408446+BeamMeUpSc0tty@users.noreply.github.com>
2019-12-04 21:52:19 -0600

    .

--- Site1.Master.designer.cs
+++ Site1.Master.designer.cs
@@ -42,22 +42,22 @@ namespace BCIS4720
         protected global::System.Web.UI.WebControls.ImageButton HomePageBttn;
 
         /// <summary>
-        /// HyperLink1 control.
+        /// ImageButton1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton1;
 
         /// <summary>
-        /// HyperLink2 control.
+        /// ImageButton2 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.HyperLink HyperLink2;
+        protected global::System.Web.UI.WebControls.ImageButton ImageButton2;
 
         /// <summary>
         /// HyperLink3 control.
